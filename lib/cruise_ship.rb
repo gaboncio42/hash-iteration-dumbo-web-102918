abd5ae522d9d07673f67b52b3dff9315e5bf2307
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-10-18 19:31:16 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,12 +7,10 @@
 # }
 
 def select_winner(passengers)
-  winner = ""
   passengers.each do |suite, name|
+    winner = ""
     if suite == :suite_a && name.start_with?("A")
       winner = name
     end
   end
 end
-
-winner
